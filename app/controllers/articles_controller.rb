0c32c694d90c31fc5c24df9855dc0100900d9d9f
Patrick M. Ryan <pmryan@us.ibm.com>
2019-03-08 16:10:30 -0500

    ready to deploy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
   def index
     @articles = Article.all
@@ -9,12 +10,12 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
   end
 
   def create
     #render plain: params[:article].inspect
-     @article = Article.new(article_params)
+    @article = Article.new(article_params)
 
     if @article.save
       flash[:notice] = "article saved"
@@ -26,7 +27,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-      @article = Article.find(params[:id])
+      #@article = Article.find(params[:id])
       if @article.update(article_params)
         flash[:notice] = "Article updated"
         redirect_to article_path(@article)
@@ -37,11 +38,11 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
   end
 
   def destroy
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article deleted"
     redirect_to articles_path
@@ -50,6 +51,10 @@ class ArticlesController < ApplicationController
   private
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 
 end
