a9c9e87511d653aa82af2b0fb42c9b0f10f70d64
Patrick M. Ryan <pmryan@us.ibm.com>
2019-03-22 15:40:57 -0400

    password stuff

---
+++ db/migrate/20190322193611_add_pwd_digest.rb
@@ -0,0 +1,6 @@
+class AddPwdDigest < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :password_digest, :string
+
+  end
+end
---
+++ g.sh
@@ -0,0 +1,5 @@
+ gem pristine executable-hooks --version 1.6.0
+ gem pristine gem-wrappers --version 1.4.0
+ gem pristine jaro_winkler --version 1.5.2
+ gem pristine psych --version 3.1.0
+ gem pristine unf_ext --version 0.0.7.5
