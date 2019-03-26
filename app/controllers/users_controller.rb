a2e87c54f8f968fc38492e9caa71448c5c5ff2e8
Patrick M. Ryan <pmryan@us.ibm.com>
2019-03-26 15:45:28 -0400

    added pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.all
+    #@users = User.all
+    @users = User.paginate(page: params[:page], per_page: 3)
   end
 
   def new
@@ -35,6 +36,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   private
