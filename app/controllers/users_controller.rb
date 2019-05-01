6548ba4254cdd4f90f546c1816f40b313ac6ebd3
Patrick M. Ryan <pmryan@us.ibm.com>
2019-05-01 18:01:33 -0400

    though lesson 133

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:edit, :update, :show]
+
   def index
     #@users = User.all
     @users = User.paginate(page: params[:page], per_page: 3)
@@ -21,11 +23,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "You account was updated successfully"
       self.redirect_to(self.articles_path)
@@ -35,7 +37,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -43,6 +45,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
